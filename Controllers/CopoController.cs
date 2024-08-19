474a9090fe4d90885d7b5ee50ae495559010986d
eduardo0804 <jesuslazbra1008@outlook.com>
2024-08-19 15:02:38 -0500

    main

---
+++ Controllers/CopoController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace appdemo_2.Controllers
+{
+    public class CopoController : Controller
+    {
+        private readonly ILogger<CopoController> _logger;
+
+        public CopoController(ILogger<CopoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
